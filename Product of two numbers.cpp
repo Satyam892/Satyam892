0efbc43fa7496b9a9b7dc932e3ee8ff6dc141597
Satyam892 <83881652+Satyam892@users.noreply.github.com>
2021-05-09 20:24:19 +0530

    Product of two numbers.cpp
    solved the program of finding a product of two numbers

---
+++ Product of two numbers.cpp
@@ -0,0 +1,20 @@
+//Q.2 Write a c++ program to find the product of two numbers entered by the user//
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    double firstnumber,secondnumber,productoftwonumbers;
+    cout<<"Enter Two numbers:";
+    cin>>firstnumber>>secondnumber;
+    productoftwonumbers=firstnumber*secondnumber;
+    cout<<" Product ="<<productoftwonumbers;
+    return 0;
+}
+
+
+
+Output:Enter Two numbers:3.4
+5.5
+ Product =18.7
